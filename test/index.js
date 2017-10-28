11777b7b1e80e979ca35fe27819f65f4b6e39654
renanbastos93 <renanbastos.tec@gmail.com>
2017-10-28 01:10:16 -0200

    [+] tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,24 @@
 const image2base64 = require('./../');
+const assert = require('assert');
+const pt = require('path')
 
-image2base64('http://bit.ly/2hfduaO')
-.then(
-    a => console.log(a)
-)
-.catch(
-    x => console.log(x)
-);
+let url = "http://jaystack.com/wp-content/uploads/2015/12/nodejs-logo-e1497443346889.png";
+let path = pt.resolve("test/nodejs.png");
+
+describe("must to be resolved the promise", function(){
+
+    it('get image of the url and convert to base64', function(){
+        image2base64(url)
+        .then(
+            data => assert(data.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{0,2}==)$/gim))
+        )
+    });
+
+    it('get image of the path and convert to base64', function(){
+        image2base64(path)
+        .then(
+            data => assert(data.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{0,2}==)$/gim))
+        )
+    })
+
+});
