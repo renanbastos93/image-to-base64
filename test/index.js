d85b62db17c07331fb94e53b30564fdd9a4f1746
renanbastos93 <renanbastos.tec@gmail.com>
2018-02-08 22:56:44 -0200

    improvement test

--- test/index.js
+++ test/index.js
@@ -6,19 +6,23 @@ let url = "http://jaystack.com/wp-content/uploads/2015/12/nodejs-logo-e149744334
 let path = pt.resolve("test/nodejs.png");
 
 describe("must to be resolved the promise", function(){
-
+    
     it('get image of the url and convert to base64', function(){
         image2base64(url)
         .then(
-            data => assert(data.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{0,2}==)$/gim))
+            (data) => {
+                assert(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{0,2}==)$/.test(data), true)
+            }
         )
     });
-
+    
     it('get image of the path and convert to base64', function(){
         image2base64(path)
         .then(
-            data => assert(data.match(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{0,2}==)$/gim))
+            (data) => {
+                assert(/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{0,2}==)$/.test(data), true)
+            }
         )
     })
-
+    
 });
